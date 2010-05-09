e99009d3b4b81bc706124284b245e696052a601c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-09 11:31:43 +0000

    mirror trunk/extensions too

--- mediawiki-mirror.sh
+++ mediawiki-mirror.sh
@@ -13,8 +13,14 @@ git svn fetch
 git svn rebase
 git push git@github.com:mediawiki/mediawiki-svn.git refs/remotes/*:refs/heads/*
 
-# Everything
+# trunk/phase3
 cd $MIRROR/mediawiki-trunk-phase3
 git svn fetch
 git svn rebase
 git push git@github.com:mediawiki/mediawiki-trunk-phase3.git master
+
+# trunk/extensions
+cd $MIRROR/mediawiki-trunk-extensions
+git svn fetch
+git svn rebase
+git push git@github.com:mediawiki/mediawiki-trunk-extensions.git master
